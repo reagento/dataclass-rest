40c0b746b8535ed5e7f4311f6e8fc0c77b3f7134
tdakkota <tanc13@yandex.ru>
2020-03-18 21:31:05 +0300

    add AsyncBaseClient

--- dataclass_rest/common.py
+++ dataclass_rest/common.py
@@ -1,6 +1,7 @@
+import string
 import sys
 from inspect import getfullargspec
-from typing import TypeVar, Callable, Any, Sequence
+from typing import TypeVar, Callable, Any, Sequence, get_type_hints
 
 if sys.version_info >= (3, 8):
     from typing import TypedDict
@@ -24,3 +25,29 @@ def create_args_class(func: Callable, skipped: Sequence[str]):
             continue
         fields[x] = s.annotations.get(x, Any)
     return TypedDict(f"{func.__name__}_Args", fields)  # type: ignore
+
+
+def get_method_classes(func: Callable, body_name: str):
+    """
+    gets result class and body class from method typehints
+    :param func: function to decorate
+    :param body_name: name of body parameter
+    :return: Result class, body class
+    """
+    hints = get_type_hints(func)
+    result_class = hints.get("return")
+    body_class = hints.get(body_name)
+    return result_class, body_class
+
+
+def get_skipped(url_format: str, body_name: str):
+    """
+    gets skipped args
+    :param url_format: API URL format string
+    :param body_name:  name of body parameter
+    :return: List of skipped args
+    """
+    parsed_format = string.Formatter().parse(url_format)
+    skipped = [x[1] for x in parsed_format]
+    skipped.append(body_name)
+    return skipped
