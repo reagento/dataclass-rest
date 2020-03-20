68a203a27459b51780f730c53fcccb553c136289
Tishka17 <tishka17@mail.ru>
2020-03-20 12:57:54 +0300

    fix missed sync

---
+++ dataclass_rest/sync.py
@@ -0,0 +1,47 @@
+import logging
+from json import JSONDecodeError
+from typing import Dict, Type, Optional
+
+from requests import RequestException
+
+from .base import BaseClient
+from .common import BT, RT
+from .errors import ApiError
+
+
+class Client(BaseClient):
+    __logger = logging.getLogger(__name__)
+
+    def request(self, *, url: str, method: str,
+                params: Optional[Dict] = None, body: Optional[BT] = None,
+                body_class: Optional[Type[BT]] = None,
+                result_class: Optional[Type[RT]] = None) -> RT:
+        url = "%s/%s" % (self.base_url, url)
+        self.__logger.debug("Sending requests to `%s` wtih %s", url, params)
+        if body_class:
+            body = self.factory.dump(body, body_class)
+        try:
+            response = self.session.request(
+                method=method,
+                url=url,
+                params=self._filter_params(params, (result_class, body_class)),
+                json=body
+            )
+            if not response.ok:
+                return self.handle_error(method, response.status_code, response)
+            result = response.json()
+            if result_class:
+                return self.factory.load(result, result_class)
+            return result
+        except RequestException as error:
+            self.__logger.error(
+                "RequestException when connecting with url: `%s`, error: `%s`",
+                url, error
+            )
+            raise ApiError("RequestException") from error
+        except JSONDecodeError as error:
+            self.__logger.error(
+                "Cannot decode response for url: `%s`, error: `%s`",
+                url, error
+            )
+            raise ApiError("Cannot decode response") from error
