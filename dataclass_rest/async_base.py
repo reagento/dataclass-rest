3c85a4b0db2197bbb038eb26711cb9e90a4350c9
dotX12 <64792903+dotX12@users.noreply.github.com>
2021-03-27 20:48:26 +0300

    Fix bool type

--- dataclass_rest/async_base.py
+++ dataclass_rest/async_base.py
@@ -1,10 +1,10 @@
 import logging
 from json import JSONDecodeError
 from typing import Dict, Type, Optional
-
+import json
 from aiohttp import ClientError, ClientResponse, ClientSession
 
-from .base import BaseClient, File
+from .base import BaseClient
 from .common import RT, BT
 from .errors import ApiError
 
@@ -14,27 +14,19 @@ class AsyncClient(BaseClient[ClientSession]):
 
     async def request(self, *, url: str, method: str,
                       params: Optional[Dict] = None, body: Optional[BT] = None,
-                      file: File = None,
                       body_class: Optional[Type[BT]] = None,
-                      result_class: Optional[Type[RT]] = None, base_url: Optional[str] = None) -> RT:
-        url, body, upload_file = self._prepare_request(
-            url=url,
-            method=method,
-            params=params,
-            body=body,
-            body_class=body_class,
-            file=file,
-            result_class=result_class,
-            base_url=base_url
-        )
-
+                      result_class: Optional[Type[RT]] = None) -> RT:
+        url = "%s/%s" % (self.base_url, url)
+        self.__logger.debug("Sending requests to `%s` wtih %s", url, params)
+        if body_class:
+            body = self.factory.dump(body, body_class)
         try:
+            params = {i: json.dumps(params[i]) if isinstance(params[i], bool) else params[i] for i in params}
             response: ClientResponse = await self.session.request(
                 method=method,
                 url=url,
                 params=self._filter_params(params, (result_class, body_class)),
-                json=body,
-                data=upload_file
+                json=body
             )
             if (response.status // 100) != 2:
                 return self.handle_error(method, response.status, response)
