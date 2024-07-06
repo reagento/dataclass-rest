59626280eba8d3841adb01212e7c37f0ca05377c
Dark04072006 <darkgpt9898@gmail.com>
2024-07-07 00:54:55 +0300

    fix type hints

--- dataclass_rest/rest.py
+++ dataclass_rest/rest.py
@@ -1,9 +1,11 @@
-from functools import partial
-from typing import Any, Dict, Optional, Callable
+from typing import Any, Dict, Optional, Callable, ParamSpec, TypeVar
 
 from .boundmethod import BoundMethod
 from .method import Method
 from .parse_func import parse_func, DEFAULT_BODY_PARAM
+
+_P = ParamSpec("_P")
+_RT = TypeVar("_RT")
 
 
 def rest(
@@ -32,8 +34,15 @@ def rest(
     return dec
 
 
-get = partial(rest, method="GET")
-post = partial(rest, method="POST")
-put = partial(rest, method="PUT")
-patch = partial(rest, method="PATCH")
-delete = partial(rest, method="DELETE")
+def _rest_method(func: Callable[_P, _RT], method: str) -> Callable[_P, _RT]:
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _RT:
+        return func(*args, **kwargs, method=method)
+
+    return wrapper
+    
+
+get = _rest_method(rest, method="GET")
+post = _rest_method(rest, method="POST")
+put = _rest_method(rest, method="PUT")
+patch = _rest_method(rest, method="PATCH")
+delete = _rest_method(rest, method="DELETE")
